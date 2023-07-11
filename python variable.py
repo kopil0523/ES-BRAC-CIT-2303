91247ddf284ae813a4c168fb85bb5000de840caa
BrownFish0523 <77118243+kopil0523@users.noreply.github.com>
2023-07-11 12:45:27 +0600

    Add files via upload

---
+++ python variable.py
@@ -0,0 +1,23 @@
+"""
+This program is about python variable.
+a container who stores different types of data so that we can access those data from it.
+
+There are some rules to declare a variable in python. They are:
+1. Variables are case sensitive. It accepts A-Z and a-z only. for example: empty and Empty are two different variables
+2. You cannot use number at the beginning of a variable name. It must start with a character or underscore(_).
+3. You cannot use any special characters like - ($%^&*) except underscore(_)
+
+There are some standard format for declaring variable: They are camel case, pascal case and snake case.
+"""
+
+# camel case variable
+
+firstName = Kopil
+
+# pascal case variable
+
+FirstName = Kopil
+
+# snake case variable
+
+First_name = Kopil
