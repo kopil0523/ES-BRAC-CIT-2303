0b0c77aeeca380a8878dbdaf87c612b8f30c7404
BrownFish0523 <77118243+kopil0523@users.noreply.github.com>
2023-07-11 13:45:40 +0600

    Add files via upload

---
+++ different print function format.py
@@ -0,0 +1,34 @@
+# there are different kind of print function
+
+fName = "Kopil"
+lName = "Uddin"
+
+print("My firstname is %s, last name is %s," %(fName, lName)) # This is called %s string format
+
+print(f"My firstname is {fName} and my last name is {lName}") # This is called f string format
+
+"""Next we will discuss about datatypes
+ there are 4 kinds of data types in python they are:
+ 1. int data types
+ 2. float data types
+ 3. string data types
+ 4. boolean data types
+"""
+
+age = 34 # this is an integer data type
+
+legnth = 5.64 # this a float type data
+
+name = "John Doe" # This is a string type data
+
+active = True # this is a boolean data
+
+# those print functions below shows the data type of a variable
+
+print(type(age))
+
+print(type(legnth))
+
+print(type(name))
+
+print(type(active))
