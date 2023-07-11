25384526e104d27a11932491cf89e340fe99743c
BrownFish0523 <77118243+kopil0523@users.noreply.github.com>
2023-07-11 12:00:07 +0600

    Add files via upload

---
+++ python print function, commenting.py
@@ -0,0 +1,34 @@
+# This is the basic python print function for printing anything
+print("Hello, World!")
+
+'''
+The next thing we will discuss is comment,
+comments are used to keep notes of a code so that another
+programmer understand the code easily
+There are three kind of Comments. They are: 
+1.Inline Comment, 
+2. Single Line Comment, 
+3. Multiple line Comment
+
+for example:
+'''
+
+print("It is raining")  # This is an example of inline comment
+
+# Let's try single line comment. It is a single line comment
+
+print("Anwar is a lawyer")
+
+"""Now
+this
+is 
+a
+ multiple line 
+ comment
+ """
+
+"""
+print("Saheda is ready with her creations")
+print("Her father is waiting for her performance.")
+"""
+print("Saheda's performance was excellent")
