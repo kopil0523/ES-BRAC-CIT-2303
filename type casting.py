6764bdf56911c4963d7037d267bb80d1cf32a50c
BrownFish0523 <77118243+kopil0523@users.noreply.github.com>
2023-07-11 14:13:27 +0600

    Add files via upload

---
+++ type casting.py
@@ -0,0 +1,24 @@
+# In this code we will run some type conversion
+
+a = 3 # this is an int data and we will print it as float
+
+print(float(a))
+
+avg = 33.4
+
+print(int(avg))
+
+# Taking input 
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"Your name is {name}, you are {age} years old")
+
+# take two numbers from user and multiply them
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = num1 * num2 
+
+print("The answer is" ,num3)
